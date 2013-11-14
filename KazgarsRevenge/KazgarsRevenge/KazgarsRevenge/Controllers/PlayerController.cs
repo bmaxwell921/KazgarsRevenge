aef60abdfe38b9ad3cdc24922061f99f10e10c73
Nate Vance <nate@vances.net>
2013-11-13 18:55:20 -0600

    working on ui ability mods

--- KazgarsRevenge/KazgarsRevenge/KazgarsRevenge/Controllers/PlayerController.cs
+++ KazgarsRevenge/KazgarsRevenge/KazgarsRevenge/Controllers/PlayerController.cs
@@ -44,6 +44,10 @@ namespace KazgarsRevenge
         double millisRunningCounter = 2000;
         double millisRunTime = 2000;
 
+        #region UI Frames
+        Texture2D icon_selected;
+        #endregion
+
         #region Ability Icons
         Texture2D melee;
         Texture2D range;
@@ -55,6 +59,9 @@ namespace KazgarsRevenge
         Texture2D healthPot;
         #endregion
 
+        #region Variables for UI
+        bool[] UISlotUsed = new bool[14];     //0-7 abilities, 8 primary, 9 secondary, 10-13 items
+        #endregion
 
         #region stats
         public enum StatType
@@ -123,6 +130,10 @@ namespace KazgarsRevenge
             texWhitePixel = Game.Content.Load<Texture2D>("Textures\\whitePixel");
             font = game.Content.Load<SpriteFont>("Verdana");
             InitDrawingParams();
+
+            #region UI Frame Load
+            icon_selected = Game.Content.Load<Texture2D>("Textures\\UI\\Frames\\icon_selected");
+            #endregion
 
             #region Ability Image Load
             melee = Game.Content.Load<Texture2D>("Textures\\UI\\Abilities\\DB");
@@ -256,6 +267,14 @@ namespace KazgarsRevenge
             millisShotArrowAttachCounter += elapsed;
             millisMelleCounter += elapsed;
             millisRunningCounter += elapsed;
+
+            #region UI Update Section
+            //reset the currently used abilities
+            for (int i = 0; i < 14; i++)
+            {
+                UISlotUsed[i] = false;
+            }
+            #endregion
 
             if (Game.IsActive)
             {
@@ -498,6 +517,24 @@ namespace KazgarsRevenge
                 groundTargetLocation = physicalData.Position;
                 targettedPhysicalData = null;
             }
+
+            //UI Abilities Used Frame
+            if (curKeys.IsKeyDown(Keys.Q)) UISlotUsed[0] = true;
+            if (curKeys.IsKeyDown(Keys.W)) UISlotUsed[1] = true;
+            if (curKeys.IsKeyDown(Keys.E)) UISlotUsed[2] = true;
+            if (curKeys.IsKeyDown(Keys.R)) UISlotUsed[3] = true;
+            if (curKeys.IsKeyDown(Keys.A)) UISlotUsed[4] = true;
+            if (curKeys.IsKeyDown(Keys.S)) UISlotUsed[5] = true;
+            if (curKeys.IsKeyDown(Keys.D)) UISlotUsed[6] = true;
+            if (curKeys.IsKeyDown(Keys.F)) UISlotUsed[7] = true;
+
+            if (curMouse.LeftButton == ButtonState.Pressed) UISlotUsed[8] = true;
+            if (curMouse.RightButton == ButtonState.Pressed) UISlotUsed[9] = true;
+
+            if (curKeys.IsKeyDown(Keys.D1)) UISlotUsed[10] = true;
+            if (curKeys.IsKeyDown(Keys.D2)) UISlotUsed[11] = true;
+            if (curKeys.IsKeyDown(Keys.D3)) UISlotUsed[12] = true;
+            if (curKeys.IsKeyDown(Keys.D4)) UISlotUsed[13] = true;
         }
 
         /// <summary>
@@ -714,6 +751,7 @@ namespace KazgarsRevenge
                 s.Draw(texWhitePixel, new Rectangle(RectEnemyHealthBar.X, RectEnemyHealthBar.Y, (int)(RectEnemyHealthBar.Width * mouseHoveredHealth.HealthPercent), RectEnemyHealthBar.Height), Color.Green);
             }
 
+            #region UI
             #region UIBase
             //Chat Pane
             s.Draw(texWhitePixel, new Rectangle(0, (int) ((maxY-444*average)), (int) (362*average), (int)(444*average)) , Color.Black*0.5f);
@@ -767,6 +805,10 @@ namespace KazgarsRevenge
 
             #endregion
 
+            #region Ability Bar Mods
+
+            #endregion
+
             //XP Area
             s.Draw(texWhitePixel, new Rectangle((int)((maxX / 2 - 311 * average)), (int)((maxY - 178 * average)), (int)(622 * average), (int)(20 * average)), Color.Brown * 0.5f);
             //Damage Tracker
@@ -794,6 +836,7 @@ namespace KazgarsRevenge
             //Fifth Player Frame Health
             s.Draw(texWhitePixel, new Rectangle((int)(74 * average), (int)(402 * average), (int)(74 * average), (int)(30 * average)), Color.Blue * 0.5f);
 
+            #endregion
             #endregion
         }
     }
