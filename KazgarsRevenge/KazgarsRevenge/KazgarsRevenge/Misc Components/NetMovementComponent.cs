64034879081e3c402e58a42b18ddeaba7612d797
Brandon <bmaxwell921@gmail.com>
2013-12-14 15:48:14 -0600

    New files needed for networked movement

---
+++ KazgarsRevenge/KazgarsRevenge/KazgarsRevenge/Misc Components/NetMovementComponent.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using BEPUphysics.Entities;
+
+namespace KazgarsRevenge
+{
+    /// <summary>
+    /// Component used to send velocity requests to the server
+    /// </summary>
+    public class NetMovementComponent : Component
+    {
+        // Initially the prevVel should be null so we're forced to update the server
+        private Vector3? prevVel;
+
+        private NetworkMessageManager nmm;
+
+        public NetMovementComponent(KazgarsRevengeGame game, GameEntity entity)
+            : base(game, entity)
+        {
+            prevVel = null;
+            nmm = game.Services.GetService(typeof(NetworkMessageManager)) as NetworkMessageManager;
+        }
+
+        /// <summary>
+        /// At each update, we check to see if the previous velocity
+        /// sent to the server is the different from the velocity
+        /// the attached entity currently has. If so, then we let the server
+        /// know what the new velocity is
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public override void Update(GameTime gameTime)
+        {
+            if (base.Game.gameState != GameState.Playing)
+            {
+                return;
+            }
+            Vector3 curVel = (entity.GetSharedData(typeof(Entity)) as Entity).LinearVelocity;
+            if (prevVel == null || DifferentEnough(curVel, prevVel))
+            {
+                prevVel = curVel;
+                nmm.SendVelocityMessage(curVel);
+            }
+        }
+
+        private Boolean DifferentEnough(Vector3 v1, Vector3? v2)
+        {
+            return v1 - v2 != Vector3.Zero;
+        }
+    }
+}
---
+++ KazgarsRevenge/KazgarsRevenge/KazgarsRevenge/Networking/Handlers/KineticHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lidgren.Network;
+
+namespace KazgarsRevenge
+{
+    public class KineticHandler : BaseHandler
+    {
+        PlayerManager pm;
+
+        public KineticHandler(KazgarsRevengeGame game)
+            : base(game)
+        {
+
+        }
+
+        /// <summary>
+        /// Receives the position data from all the other players,
+        /// let's the player manager know to update them
+        /// </summary>
+        /// <param name="nim"></param>
+        public override void Handle(NetIncomingMessage nim)
+        {
+            
+        }
+    }
+}
