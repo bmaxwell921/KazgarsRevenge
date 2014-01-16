618da3304ea5269727265bda8fa561d0c7c14116
Brandon <bmaxwell921@gmail.com>
2014-01-16 12:10:43 -0600

    Continued working on the UI

--- KRChunkEditor/src/main/kazgarsrevenge/ui/Main.java
+++ KRChunkEditor/src/main/kazgarsrevenge/ui/Main.java
@@ -1,12 +1,18 @@
 package main.kazgarsrevenge.ui;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.EventQueue;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import main.kazgarsrevenge.ui.panels.ImageDescriptionPanel;
+import main.kazgarsrevenge.ui.panels.SidePanel;
+import main.kazgarsrevenge.util.ImageLoader;
 
 public class Main {
-	
-	private JFrame frame;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -17,16 +23,41 @@ public class Main {
 	}
 	
 	public Main() {
+		ImageLoader.loadImages();
 		createAndShowGUI();
 	}
 	
 	public void createAndShowGUI() {
-		frame = new JFrame();
+		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Kazgar's Revenge Chunk Editor");
 		// Add panels
 		
+		setUpFrame(frame);
+	}
+	
+	private void setUpFrame(JFrame frame) {
+		Container content = frame.getContentPane();		
+		content.setLayout(new BorderLayout());
+		
+		JPanel sidePanels = new JPanel();
+		sidePanels.setLayout(new BoxLayout(sidePanels, BoxLayout.Y_AXIS));
+		sidePanels.add(setUpRoomsPanel());
+//		sidePanels.add(new SidePanel());
+		
+		content.add(sidePanels, BorderLayout.PAGE_END);
+		
 		frame.pack();
 		frame.setVisible(true);
 	}
+	
+	private SidePanel setUpRoomsPanel() {
+		SidePanel roomsPanel = new SidePanel("Rooms");
+		for (String name : ImageLoader.getImageNames()) {
+			JPanel imgDesc = new ImageDescriptionPanel(name, ImageLoader.getImage(name));
+			roomsPanel.addPanel(imgDesc);
+		}
+		return roomsPanel;
+	}
+	
 }
---
+++ KRChunkEditor/src/main/kazgarsrevenge/ui/panels/ImageDescriptionPanel.java
@@ -0,0 +1,42 @@
+package main.kazgarsrevenge.ui.panels;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * Panel for displaying the image for a single room and its name
+ * @author Brandon
+ *
+ */
+public class ImageDescriptionPanel extends JPanel {
+
+	/**
+	 * Version 1!
+	 */
+	private static final long serialVersionUID = 1L;
+
+	// The image associated with this room
+	private Image roomImage;
+	
+	// The name of this room
+	private String roomName;
+	
+	public ImageDescriptionPanel(String roomName, Image roomImage) {
+		this.roomName = roomName;
+		this.roomImage = roomImage;
+		initialize();
+	}
+	
+	private void initialize() {
+		JLabel imageButton = new JLabel(new ImageIcon(roomImage));
+		this.add(imageButton);
+		// TODO register an action listener
+		
+		JLabel roomNameLabel = new JLabel(roomName);
+		this.add(roomNameLabel);
+	}
+}
