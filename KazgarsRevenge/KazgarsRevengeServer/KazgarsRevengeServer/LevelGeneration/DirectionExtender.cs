b6cbf0bdadee2cb7c9e8e4e9acb82cf2de1c2b9e
Brandon <bmaxwell921@gmail.com>
2014-02-04 16:06:03 -0600

    New stuff that doesn't matter now

---
+++ KazgarsRevenge/KazgarsRevengeServer/KazgarsRevengeServer/LevelGeneration/DirectionExtender.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KazgarsRevenge;
+
+namespace KazgarsRevengeServer
+{
+    public class DirectionExtender
+    {
+        public static Direction toDirection(char dir)
+        {
+            if (dir == 'N' || dir == 'n')
+            {
+                return Direction.NORTH;
+            }
+            if (dir == 'S' || dir == 's')
+            {
+                return Direction.SOUTH;
+            }
+            if (dir == 'E' || dir == 'e')
+            {
+                return Direction.EAST;
+            }
+            if (dir == 'W' || dir == 'w')
+            {
+                return Direction.WEST;
+            }
+            throw new ArgumentException("Unknown Direction char: " + dir);
+        }
+    }
+}
