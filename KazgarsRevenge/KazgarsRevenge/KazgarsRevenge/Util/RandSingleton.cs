72458a1fe5945515aa0973837dea16d2f41730ab
Brandon <bmaxwell921@gmail.com>
2014-02-05 19:12:02 -0600

    Created singleton for all our random needs

---
+++ KazgarsRevenge/KazgarsRevenge/KazgarsRevenge/Util/RandSingleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KazgarsRevenge
+{
+    public class RandSingleton
+    {
+        // Teh instance!
+        private static Random instance;
+
+        public static Random Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Random();
+                }
+
+                return instance;
+            }
+        }
+    }
+}
