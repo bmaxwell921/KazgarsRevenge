576abf52273042c93363ce7db686ac2b2a0639a8
Brandon <bmaxwell921@gmail.com>
2014-02-18 12:16:59 -0600

    Removed debugging code

--- KazgarsRevenge/KazgarsRevenge/KazgarsRevenge/Controllers/Alive/LocalPlayerController.cs
+++ KazgarsRevenge/KazgarsRevenge/KazgarsRevenge/Controllers/Alive/LocalPlayerController.cs
@@ -105,13 +105,10 @@ namespace KazgarsRevenge
         KeyboardState prevKeys = Keyboard.GetState();
         bool guiClick = false;
 
-        int count = 0;
         public override void Update(GameTime gameTime)
         {
             // TODO Remove
             Entity sharedData = Entity.GetSharedData(typeof(Entity)) as Entity;
-            if (count++ % 30 == 0)
-                Console.WriteLine(sharedData.Position);
             curMouse = Mouse.GetState();
             curKeys = Keyboard.GetState();
             double elapsed = gameTime.ElapsedGameTime.TotalMilliseconds;
