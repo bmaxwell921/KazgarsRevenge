299f3f92fe6c7f4d4d52d95913845b83b4ee798f
Brandon <bmaxwell921@gmail.com>
2014-03-03 22:40:41 -0600

    Added classes to better organize texture loading

---
+++ KazgarsRevenge/KazgarsRevenge/KazgarsRevenge/Common/TextureStrings.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KazgarsRevenge
+{
+    /// <summary>
+    /// While this may look weird, I really like this model. When you need the
+    /// LOTS of coins string you do: TextureStrings.UI.Items.Coins.LOTS. Makes everything nice and organized
+    /// </summary>
+    public class TextureStrings
+    {
+        public static readonly string WHITE = @"white";
+        public static readonly string WHITE_PIX = @"Textures\whitePixel";
+        public static readonly string WHITE_CURSOR = @"Textures\whiteCursor";
+        public class UI
+        {
+            public static readonly string CHARGE_BAR_FRONT = @"Textures\UI\chargeBarFront";
+
+            public class Items
+            {
+                public class Coins
+                {
+                    public static readonly string FEW = @"Textures\UI\Items\gold1";
+                    public static readonly string SOME = @"Textures\UI\Items\gold2";
+                    public static readonly string LOTS = @"Textures\UI\Items\gold3";
+                }
+
+                public class Potions
+                {
+                    public static readonly string HEALTH = @"Textures\UI\Items\HP";
+                    public static readonly string SUPER_HEALTH = @"Textures\UI\Items\UNIMPLEMENTED";
+                    public static readonly string INSTA_HEALTH = @"Textures\UI\Items\UNIMPLEMENTED";
+                    public static readonly string LUCK = @"Textures\UI\Items\UNIMPLEMENTED";
+                    public static readonly string INVISIBILITY = @"Textures\UI\Items\UNIMPLEMENTED";
+                }
+
+                public class Armor
+                {
+
+                }
+            }
+
+            public class Abilities
+            {
+                public class Range
+                {
+                    public static readonly string SNIPE = @"Textures\UI\Abilities\LW";
+                    public static readonly string HEART_STRIKE = @"Textures\UI\Abilities\HS";
+                    public static readonly string ICE_CLAW_PRI = @"Textures\whitePixel";
+                    public static readonly string OMNI_SHOT = @"Textures\UI\Abilities\I4";
+                    public static readonly string ADREN_RUSH = @"Textures\UI\Abilities\I4";
+                    public static readonly string LEECH_ARROWS = @"Textures\UI\Abilities\HS";
+                    public static readonly string LOOSE_CANNON = @"Textures\UI\Abilities\I4";
+                    public static readonly string MAKE_IT_RAIN = @"Textures\UI\Abilities\LW";
+                    public static readonly string FLASH_BOMB = @"Textures\UI\Abilities\BR";
+                    public static readonly string TAR_BOMB = @"Textures\UI\Abilities\BR";
+                    public static readonly string GRAP_HOOK = @"Textures\UI\Abilities\I4";
+                    public static readonly string CHAIN_SPEAR = @"Textures\UI\Abilities\I4";
+                    public static readonly string MOLT_BOLT = @"Textures\UI\Abilities\I4";
+                }
+            }
+
+            public class Frames
+            {
+                public static readonly string ICON_SEL = @"Textures\UI\Frames\icon_selected";
+                public static readonly string HEALTH_BAR = @"Textures\UI\Frames\health_bar";
+                public static readonly string RIGHT_ARROW = @"Textures\UI\Frames\rightArrow";
+                public static readonly string LEFT_ARROW = @"Textures\UI\Frames\leftArrow";
+                public static readonly string HELMET = @"Textures\UI\Frames\helmetIcon";
+            }
+        }
+
+        public class BillBoards
+        {
+            public static readonly string BLOB = @"Textures\Billboards\blob";
+            public static readonly string GRND_IND = @"Textures\Billboards\groundIndicator";
+            public static readonly string CHAIN = @"Textures\Billboards\chain";
+            public static readonly string ROPE = @"Textures\Billboards\rope";
+        }
+
+        public class Menu
+        {
+            public static readonly string BACKGRND = @"Textures\Menu\menuBackground";
+        }
+    }
+}
---
+++ KazgarsRevenge/KazgarsRevenge/KazgarsRevenge/Util/Texture2DUtil.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using System.Threading;
+
+namespace KazgarsRevenge
+{
+    /// <summary>
+    /// Utility responsible for handling loading and 
+    /// distributing Texture2D assets. Singleton cause it's the best.
+    /// 
+    /// TODO could implement this as LRU if we need to worry about memory
+    /// </summary>
+    public class Texture2DUtil
+    {
+
+        #region Singleton Stuff
+        private static Texture2DUtil instance;
+
+        public static Texture2DUtil Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Texture2DUtil();
+                }
+                return instance;
+            }
+        }
+
+        private Texture2DUtil()
+        {
+            this.textures = new Dictionary<string, Texture2D>();
+            mutex = new Mutex();
+        }
+        #endregion
+
+        // All the loaded textures
+        private IDictionary<string, Texture2D> textures;
+
+        private ContentManager cm;
+
+        // Just in case we need to multi-thread it
+        private Mutex mutex;
+
+        /// <summary>
+        /// Sets the content manager for the Util to use when loading
+        /// </summary>
+        /// <param name="content"></param>
+        public void SetContent(ContentManager content)
+        {
+            cm = content;
+        }
+        /// <summary>
+        /// Loads textures that are most likely to be used - abilities, coins, potions
+        /// </summary>
+        public void PreemptiveLoad()
+        {
+            PerformLoad(TextureStrings.UI.Items.Coins.FEW);
+            PerformLoad(TextureStrings.UI.Items.Coins.SOME);
+            PerformLoad(TextureStrings.UI.Items.Coins.LOTS);
+            PerformLoad(TextureStrings.UI.Items.Potions.HEALTH);
+        }
+        
+        // Performs a thread safe load
+        private void PerformLoad(string toLoad)
+        {
+            mutex.WaitOne();
+            try
+            {
+                textures[toLoad] = cm.Load<Texture2D>(toLoad);
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
+        }
+
+        /// <summary>
+        /// Gets the requested texture. The name should come from the TextureStrings class
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public Texture2D GetTexture(string name)
+        {
+            if (!textures.ContainsKey(name))
+            {
+                PerformLoad(name);
+            }
+            return textures[name];
+        }
+    }
+}
